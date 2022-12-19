7f96a69687c2ff9ca3fb9530d3dff042245e7a55
Mark Kirby <mark@kirbyware.com>
2022-12-19 12:48:23 -0500

    add jest example

---
+++ src/__tests__/sum.test.js
@@ -0,0 +1,10 @@
+const sum = require('../sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 4 + 1 to equal 5', () => {
+    expect(sum(4, 1)).toBe(5);
+  });
+  

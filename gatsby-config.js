c1ca4fbbcc178067ad10ecdba00b2fe64417df69
Mark Kirby <mark@kirbyware.com>
2022-12-21 16:58:58 -0500

    update google analystics tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-7D5L427VSS",
+        trackingId: "G-467252NXZB",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional

a7d47033396b7648523b6611fa83882f9127ca1a
Mark Kirby <mark@kirbyware.com>
2022-12-22 11:42:25 -0500

    improve date formatting

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/blog-list-item.tsx
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import * as React from "react"
+import { jsx, Box } from "theme-ui"
+import { format } from "date-fns";
+import { Link } from "gatsby"
+import ItemTags from "@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags"
+
+type BlogListItemProps = {
+  post: {
+    slug: string
+    title: string
+    date: string
+    excerpt: string
+    description: string
+    timeToRead?: number
+    tags?: {
+      name: string
+      slug: string
+    }[]
+  }
+  showTags?: boolean
+}
+
+const dateYMD = new Date("2022-11-21 00:01")
+
+const BlogListItem = ({ post, showTags = true }: BlogListItemProps) => (
+  <Box mb={4}>
+    <Link to={post.slug} sx={(t) => ({ ...t.styles?.a, fontSize: [1, 2, 3], color: `text` })}>
+      {post.title}
+    </Link>
+    <p sx={{ color: `secondary`, mt: 1, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
+      <time datetime={post.date}>{format(new Date(post.date.split(".").reverse().join("-").concat("T01:01:00")), 'MMM dd, yyyy')}</time>
+      {post.tags && showTags && (
+        <React.Fragment>
+          {` — `}
+          <ItemTags tags={post.tags} />
+        </React.Fragment>
+      )}
+    </p>
+  </Box>
+)
+
+export default BlogListItem
